3a39988100272bedf3af4a92f4964bb8226a716a
zhupa <382211145@qq.com>
2018-01-17 17:46:29 +0800

    认证中心,API中心,客户端,

---
+++ IdentityClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel.Client;
+using System;
+
+namespace IdentityClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Do();
+            Console.Read();
+        }
+
+        public static async void Do()
+        {
+            // 从元数据中发现端口
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.Read();
+        }
+    }
+}
