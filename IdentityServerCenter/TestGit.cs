9e676c9b36c30b0fbf7ecb34d116b12f2ba3dcc7
zhupa <382211145@qq.com>
2018-01-23 19:25:27 +0800

    dev1_update2

--- IdentityServerCenter/TestGit.cs
+++ IdentityServerCenter/TestGit.cs
@@ -11,6 +11,7 @@ namespace IdentityServerCenter
         /// 修改一处地方,然后看看编辑是不是会把当前的修改覆盖掉
         /// </summary>
         public int dev1_update1 { get; set; }
+        public int dev1_update2 { get; set; }
 
 
     }
