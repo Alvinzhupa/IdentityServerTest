7922cabe7ccfb4c6f3e33e96c7837f913d8f86db
zhupa <382211145@qq.com>
2018-01-23 19:25:08 +0800

    提交到dev1,update1 第一个修改

--- IdentityServerCenter/TestGit.cs
+++ IdentityServerCenter/TestGit.cs
@@ -10,6 +10,8 @@ namespace IdentityServerCenter
         /// <summary>
         /// 修改一处地方,然后看看编辑是不是会把当前的修改覆盖掉
         /// </summary>
-        public int aabbbbbb { get; set; }
+        public int dev1_update1 { get; set; }
+
+
     }
 }
