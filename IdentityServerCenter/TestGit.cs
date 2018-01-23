7e3033a97eb3ed2aa6d7d8f14ebacc905391b6c6
zhupa <382211145@qq.com>
2018-01-23 19:16:37 +0800

    提交到当前dev1的本地仓库,并不推送到远程仓库

--- IdentityServerCenter/TestGit.cs
+++ IdentityServerCenter/TestGit.cs
@@ -7,5 +7,9 @@ namespace IdentityServerCenter
 {
     public class TestGit
     {
+        /// <summary>
+        /// 修改一处地方,然后看看编辑是不是会把当前的修改覆盖掉
+        /// </summary>
+        public int aabbbbbb { get; set; }
     }
 }
